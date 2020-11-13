8d5bdba8fd0512c7562e544438597e5c401d9f1f
chjoguer <cjguerrero_garcia@outlook.com>
2020-11-12 23:32:41 -0500

    Marriage Customer

--- ec.edu.espol.workshops.second/src/test/resources/ec/edu/espol/workshops/second/StepDefs.java
+++ ec.edu.espol.workshops.second/src/test/resources/ec/edu/espol/workshops/second/StepDefs.java
@@ -6,22 +6,46 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
 public class StepDefs {
-	private String today;
-	private String actualAnswer;
-	@Given("today is Sunday")
-	public void today_is_Sunday() {
-	today = "Sunday";
+	String marriage;
+	String sex;
+	String notMarried;
+	String married;
+	String state;
+	@Given("customer is not married")
+	public void customer_is_not_married() {
+		this.marriage = "not married";
+		
 	}
-	@Given("today is {string}")
-	public void today_is(String today) {
-	this.today = today;
+	
+	@When("i ask you for you married")
+	public void i_ask_you_for_you_married() {
+		this.married = "married";
 	}
-	@When("I ask whether it's Friday yet")
-	public void i_ask_whether_it_s_Friday_yet() {
-	actualAnswer = "Its Friday";
+	
+	@Then("i should get married")
+	public void i_should_get_married() {
+		assertEquals("married", this.married);
 	}
-	@Then("I should be told {string}")
-	public void i_should_be_told(String expectedAnswer) {
-	assertEquals(expectedAnswer, actualAnswer);
+	
+	@When("i ask you for you not married")
+	public void i_ask_you_for_you_not_married() {
+		this.notMarried = "not married";
+	}
+
+	@Then("i should get not married")
+	public void i_should_get_not_married() {
+		assertEquals("not married", this.notMarried);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
