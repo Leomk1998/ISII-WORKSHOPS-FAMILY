8cff41e8c4ef77a3e65707b31b71f32f1cb0f140
Luis Carrasco <cluis0152@gmail.com>
2020-11-12 23:49:07 -0500

    commit

--- ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/StepDefs.java
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/StepDefs.java
@@ -6,9 +6,40 @@ import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
 public class StepDefs {
+	String marriage;
+	String sex;
+	String notMarried;
+	String married;
+	String state;
 	private String today;
 	private String gender;
 	private String actualAnswer;
+	@Given("customer is not married")
+	public void customer_is_not_married() {
+		this.marriage = "not married";
+
+	}
+	
+	@When("i ask you for you married")
+	public void i_ask_you_for_you_married() {
+		this.married = "married";
+	}
+	
+	@Then("i should get married")
+	public void i_should_get_married() {
+		assertEquals("married", this.married);
+	}
+	
+	@When("i ask you for you not married")
+	public void i_ask_you_for_you_not_married() {
+		this.notMarried = "not married";
+	}
+
+	@Then("i should get not married")
+	public void i_should_get_not_married() {
+		assertEquals("not married", this.notMarried);
+	}
+	
 	@Given("today is Sunday")
 	public void today_is_Sunday() {
 	today = "Sunday";
