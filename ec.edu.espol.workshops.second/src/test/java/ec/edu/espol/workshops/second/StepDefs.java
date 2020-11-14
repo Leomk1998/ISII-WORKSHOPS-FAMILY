62cfb60ca67ccadf5b7e1105fce2f839764e29f4
chjoguer <cjguerrero_garcia@outlook.com>
2020-11-13 19:18:27 -0500

    Merge

--- ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/StepDefs.java
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/StepDefs.java
@@ -40,22 +40,6 @@ public class StepDefs {
 		assertEquals("not married", this.notMarried);
 	}
 	
-	@Given("today is Sunday")
-	public void today_is_Sunday() {
-	today = "Sunday";
-	}
-	@Given("today is {string}")
-	public void today_is(String today) {
-	this.today = today;
-	}
-	@When("I ask whether it's Friday yet")
-	public void i_ask_whether_it_s_Friday_yet() {
-	actualAnswer = "Its Friday";
-	}
-	@Then("I should be told {string}")
-	public void i_should_be_told(String expectedAnswer) {
-	assertEquals(expectedAnswer, actualAnswer);
-	}
 	
 	@Given("I am other")
 	public void f() {
