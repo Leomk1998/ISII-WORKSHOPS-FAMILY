6a0bd95bf3ecb2425c9b1d396f54b9c7917ffa96
chjoguer <cjguerrero_garcia@outlook.com>
2020-12-23 00:32:18 -0500

    PathCoverageTest Finalizado

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -3,33 +3,48 @@ package ec.edu.espol.workshops.second;
 import java.util.Scanner;
 
 public class CarInsurance {
-	public static void main(String[] args) {
-		System.out.print("Ingrese su edad:");
-	    String entradaTeclado = "";
-	    Scanner entradaEscaner = new Scanner(System.in);
-	    entradaTeclado = entradaEscaner.nextLine();
-	    int edad = Integer.parseInt(entradaTeclado);
-	    System.out.print("Ingrese su sexo(M/F):");
-	    String sexo = entradaEscaner.nextLine();
-	    entradaTeclado = "";
-	    System.out.print("Ingrese si esta casado o no(married/not married):");
-	    String casado = entradaEscaner.nextLine();
-	    entradaTeclado = "";
-	    System.out.println(casado.equals("not married"));
-	    int prima = 600; //Comentariox2
-	    if(sexo.equals("M") && casado.equals("not married") && edad<25) {
-	    	prima+=1500;
-	    }if(sexo.equals("F") && casado.equals("married")) {
-	    	prima-=200;
-	    }if(edad<65 && edad>45) {
-	    	prima-=100;
-	    }if(edad>80) {
-	    	 System.out.println("No se vender seguros para mayores de 80");
-	    	 prima=-1;
-	    }
-	    System.out.println("El valor de la prima es "+prima);
-	    
-	}
 	
+	public static int carInsurance(String sexo,String casado,int edad) {
+			int prima = 600;
+		    if(sexo.equals("M") && casado.equals("not married") && edad<25) {
+		    	prima+=1500;
+		    }if(sexo.equals("F") && casado.equals("married")) {
+		    	prima-=200;
+		    }if(edad<65 && edad>45) {
+		    	prima-=100;
+		    }if(edad>80) {
+		    	 System.out.println("No se vender seguros para mayores de 80");
+		    	 prima=-1;
+		    }
+		    return prima;		
+		}
+//	public static void main(String[] args) {
+//		System.out.print("Ingrese su edad:");
+//	    String entradaTeclado = "";
+//	    Scanner entradaEscaner = new Scanner(System.in);
+//	    entradaTeclado = entradaEscaner.nextLine();
+//	    int edad = Integer.parseInt(entradaTeclado);
+//	    System.out.print("Ingrese su sexo(M/F):");
+//	    String sexo = entradaEscaner.nextLine();
+//	    entradaTeclado = "";
+//	    System.out.print("Ingrese si esta casado o no(married/not married):");
+//	    String casado = entradaEscaner.nextLine();
+//	    entradaTeclado = "";
+//	    System.out.println(casado.equals("not married"));
+//	    int prima = 600; //Comentariox2
+//	    if(sexo.equals("M") && casado.equals("not married") && edad<25) {
+//	    	prima+=1500;
+//	    }if(sexo.equals("F") && casado.equals("married")) {
+//	    	prima-=200;
+//	    }if(edad<65 && edad>45) {
+//	    	prima-=100;
+//	    }if(edad>80) {
+//	    	 System.out.println("No se vender seguros para mayores de 80");
+//	    	 prima=-1;
+//	    }
+//	    System.out.println("El valor de la prima es "+prima);
+//	    
+//	}
+//	
 	
 }
--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Customer.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Customer.java
@@ -4,58 +4,59 @@ package ec.edu.espol.workshops.second;
 * Comments
 * @author Christian Guerrero
 */
-public class Customer {
-	public String sexo;//Variable sexo
-	public String casado;//Variable string
-	private int edad=0;
-	/**
-	* Add Constructor
-	*/
-	public Customer() {
-		
-	}
-	
-	
-	public String getSexo() {
-		return sexo;
-	}
-
-
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-
-
-	public String getCasado() {
-		return casado;
-	}
-
-
-
-	public void setCasado(String casado) {
-		this.casado = casado;
-	}
-
-
-
-	public int getEdad() {
-		return edad;
-	}
-
-
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Customer [sexo=" + sexo + ", casado=" + casado + ", edad=" + edad + "]";
-	}
-	
-	
-}
+//
+//public class Customer {
+//	public String sexo;//Variable sexo
+//	public String casado;//Variable string
+//	private int edad=0;
+//	/**
+//	* Add Constructor
+//	*/
+//	public Customer() {
+//		
+//	}
+//	
+//	
+//	public String getSexo() {
+//		return sexo;
+//	}
+//
+//
+//
+//	public void setSexo(String sexo) {
+//		this.sexo = sexo;
+//	}
+//
+//
+//
+//	public String getCasado() {
+//		return casado;
+//	}
+//
+//
+//
+//	public void setCasado(String casado) {
+//		this.casado = casado;
+//	}
+//
+//
+//
+//	public int getEdad() {
+//		return edad;
+//	}
+//
+//
+//
+//	public void setEdad(int edad) {
+//		this.edad = edad;
+//	}
+//
+//
+//
+//	@Override
+//	public String toString() {
+//		return "Customer [sexo=" + sexo + ", casado=" + casado + ", edad=" + edad + "]";
+//	}
+//	
+//	
+//}
